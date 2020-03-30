bec7c1c1eb30be286a8c56cf4db10cce55cd9a24
Harry Krautschneider <harry2.krautschneider@live.uwe.ac.uk>
2020-03-30 16:00:25 +0100

    Upload of movie completed

--- models/movies.js
+++ models/movies.js
@@ -3,20 +3,31 @@ const axios = require('axios').default;
 
 exports.getMovie = function(req, res) {
 
+
     const movieTitle = req.params.movieName
-    axios({
-        method: 'post',
-        url: 'http://www.omdbapi.com/?t=' + movieTitle + '&apikey=' + process.env.OMDB_KEY,
-        responseType: 'json'
-    })
-    .then(function (response) {
-        res.render('../views/pages/movie.ejs', {
-            movie: response.data
+
+
+    let sql = "SELECT * FROM movies WHERE title = ?"
+    connection.query(sql, movieTitle, function(error, results, fields) {
+        if (error) throw error;
+
+        axios({
+            method: 'post',
+            url: 'http://www.omdbapi.com/?t=' + movieTitle + '&apikey=' + process.env.OMDB_KEY,
+            responseType: 'json'
         })
+        .then(function (response) {
+            res.render('../views/pages/movie.ejs', {
+                movie: response.data,
+                file: results
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     })
-    .catch(function (error) {
-        console.log(error);
-    });
+
+
 
 
 
